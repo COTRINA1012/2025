90a06672c332bb18c018cdacbc25ddf05d2c5b2c
COTRINA1012 <167833358+COTRINA1012@users.noreply.github.com>
2025-01-09 18:29:03 +0900

    Update ex1-3.js

--- ex1-3.js
+++ ex1-3.js
@@ -1,2 +1,2 @@
-function imgchange1(){document.getElementById('logo1').src='./image/hellokity2.png';}
-function imgmotosu1(){document.getElementById('logo1').src='./image/hellokity1.png';}
+function imgchange1(){document.getElementById('logo1').src='./image/hellokity1.png';}
+function imgmotosu1(){document.getElementById('logo1').src='./image/hellokity2.png';}
