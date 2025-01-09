c4fc0975f0fb4aa8dfd648e8ef174da36d784ddf
COTRINA1012 <167833358+COTRINA1012@users.noreply.github.com>
2025-01-09 18:46:39 +0900

    Update ex1-2.js

--- ex1-2.js
+++ ex1-2.js
@@ -1,7 +1,7 @@
 function hyoji() {
 var str = "あなたの親友になりたいな！";
 var cnt = document.timer.moji.value.length;
-if ( cnt < 8 ) {
+if ( cnt < 12 ) {
 document.timer.moji.value = str.substr(0,cnt+1); } else {
 document.timer.moji.value = ""; }
 setTimeout("hyoji()",500); }
